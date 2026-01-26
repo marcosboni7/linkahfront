4ac2fc8c277fa78cb8c63593d0f1cf12a363a376
linkah <developer@linkah.com.br>
2026-01-26 18:47:19 -0300

    Forçando atualização das URLs

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-  // O Next.js identifica que esta é a página raiz e joga o usuário para /login
+ 
   redirect('/login');
 }
