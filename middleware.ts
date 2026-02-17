e44b7adba92794d3bb301634c687e4586492112b
linkah <developer@linkah.com.br>
2026-02-17 10:17:43 -0300

    Teste

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/request';
+
+export function middleware(request: NextRequest) {
+  // Se o usuário acessar a raiz "/"
+  if (request.nextUrl.pathname === '/') {
+    // Redireciona ele para a página "about"
+    return NextResponse.redirect(new URL('/about', request.url));
+  }
+}
+
+// Configura para o middleware rodar apenas na página inicial
+export const config = {
+  matcher: '/',
+};
